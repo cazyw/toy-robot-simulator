f7e873dfbcff728800f54b15890ddba1aaa4b4d0
cazyw <kazeisc@gmail.com>
2017-09-10 14:01:46 +1000

    Add case to handle where the input is REPORT

--- tests/test.rb
+++ tests/test.rb
@@ -6,49 +6,106 @@ class TestRobot < Test::Unit::TestCase
     # test the table is set up 5 x 5
     def test_table_grid
         t = Table.new
-        t == [["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"]]
+        t.table == [["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"]]
         assert_equal(true, t.is_a?(Table))
         assert_equal([-1, -1], t.pos)
         assert_equal("directionless", t.face)
     end
 
     def test_input_place_valid
-        # t = Table.new
+        t = Table.new
         input = "PLACE 0,0,NORTH"
-        result = get_command(input)
-        assert_equal(true, result.is_a?(Table))       
+        result = t.get_command(input)
+        assert_equal(true, result.is_a?(Table))
+        assert_equal([0, 0], result.pos)
+        assert_equal("NORTH", result.face)       
     end
 
     def test_input_place_invalid_direction
-        # t = Table.new
+        t = Table.new
         input = "PLACE 0,0,UP"
-        result = get_command(input)
+        result = t.get_command(input)
         assert_equal("invalid command", result)       
     end
 
     def test_input_place_invalid_arguments
-        # t = Table.new
+        t = Table.new
         input = "PLACE 0,,UP"
-        result = get_command(input)
+        result = t.get_command(input)
         assert_equal("invalid command", result)       
     end
 
-    def test_input_place_places_robot_0_0
-        input = "PLACE 0,0,NORTH"
-        result = get_command(input)
-        assert_equal([0,0], result.pos)       
+
+    def test_input_place_places_robot_3_2_west
+        t = Table.new
+        input = "PLACE 3,2,WEST"
+        result = t.get_command(input)
+        assert_equal(true, result.is_a?(Table))
+        assert_equal([3, 2], result.pos)
+        assert_equal("WEST", result.face)      
     end
 
-    def test_input_place_places_robot_3_2
-        input = "PLACE 3,2,NORTH"
-        result = get_command(input)
-        assert_equal([3,2], result.pos)       
-        assert_equal("NORTH", result.face)       
-    end
-
-    def test_input_place_ignores_robot_5_0
+    def test_input_place_ignores_robot_5_0_north
+        t = Table.new
         input = "PLACE 5,0,NORTH"
-        result = get_command(input)
+        result = t.get_command(input)
         assert_equal("invalid command", result)           
+    end
+    def test_input_report_robot_not_set
+        t = Table.new
+        input = "REPORT"
+        result = t.get_command(input)
+        assert_equal("position: [-1, -1] facing directionless", result)         
+    end
+
+    def test_input_report_robot_set_to_3_3
+        t = Table.new
+        input = "PLACE 3,2,NORTH"
+        t = t.get_command(input)
+        input = "PLACE 3,3,SOUTH"
+        t = t.get_command(input)
+        input = "REPORT"
+        result = t.get_command(input)
+        assert_equal("position: [3, 3] facing SOUTH", result)        
+    end
+
+    def test_input_report_robot_valid_moves
+        t = Table.new
+        input = "PLACE 3,2,NORTH"
+        t = t.get_command(input)
+        input = "MOVE"
+        t = t.get_command(input)
+        input = "MOVE"
+        t = t.get_command(input)
+        input = "REPORT"
+        result = t.get_command(input)
+        assert_equal("position: [3, 4] facing NORTH", result)        
+    end
+
+    def test_input_report_robot_invalid_moves_ignored
+        t = Table.new
+        input = "PLACE 3,2,NORTH"
+        t = t.get_command(input)
+        input = "MOVE"
+        t = t.get_command(input)
+        input = "MOVE"
+        t = t.get_command(input)
+        input = "MOVE"
+        t = t.get_command(input)
+        input = "MOVE"
+        t = t.get_command(input)
+        input = "REPORT"
+        result = t.get_command(input)
+        assert_equal("position: [3, 4] facing NORTH", result)        
+    end
+    def test_input_report_robot_invalid_moves_ignored_2
+        t = Table.new
+        input = "PLACE 4,2,EAST"
+        t = t.get_command(input)
+        input = "MOVE"
+        t = t.get_command(input)
+        input = "REPORT"
+        result = t.get_command(input)
+        assert_equal("position: [4, 2] facing EAST", result)        
     end
 end
--- toyRobot.rb
+++ toyRobot.rb
@@ -2,6 +2,7 @@
 
 class Table
     attr_reader :table, :pos, :face
+
     # Setup the table
     def initialize
         @table = Array.new(5) { Array.new(5, "x") }
@@ -12,46 +13,99 @@ class Table
     end
 
     def place_robot(col, row, f)
+
+        # there's already a game underway
+        if (@pos[0] >= 0)
+            @table[(@pos[1]-4).abs][@pos[0]] = "x"
+        end
+
         # to account for starting in bottom left corner
         modrow = (row-4).abs
         @table[modrow][col] = "O"
         @pos = [col, row]
-        @face = f.downcase
+        @face = f
+        printTable  
+        return self
+    end
+
+    def move
+        # if negative, robot has not been placed
+        if (@pos[0] < 0)
+            return self
+        end
+
+        # values to change the robot on the board
+        direction = {
+            "NORTH" => [-1, 0], 
+            "SOUTH" => [1, 0],
+            "EAST" => [0, 1],
+            "WEST" => [0, -1]
+        }
+        moveBy = direction[@face]
+        
+        # check if move takes it out of bounds
+        if (@pos[0] + moveBy[1] > 4 || @pos[1] - moveBy[0] > 4 ||
+            @pos[0] + moveBy[1] < 0 || @pos[1] - moveBy[0] < 0)
+            puts "out of bounds"
+        else
+            @table[(@pos[1]-4).abs][@pos[0]] = "x"
+            @pos[1] -= moveBy[0]
+            @pos[0] += moveBy[1]
+            @table[(@pos[1]-4).abs][@pos[0]] = "O"
+            puts "valid move"
+        end
         printTable
+        return self
     end
 
     def printTable
         puts @table.map { |x| x.join(" ") }
         puts "position: #{@pos} facing #{@face}"
+        return "position: #{@pos} facing #{@face}"
+    end
+
+    def get_command(command)
+        command.upcase!
+        case command
+            when /^PLACE *([0-4]), *([0-4]), *(NORTH|SOUTH|EAST|WEST)$/i 
+                match = command.match(/^PLACE *([0-4]), *([0-4]), *(NORTH|SOUTH|EAST|WEST)$/i)
+                col, row, face = match.captures
+                place_robot(col.to_i, row.to_i, face)
+                puts "ok"
+                return self
+            when "MOVE"
+                move
+                return self
+            when "REPORT"
+                printTable
+            else
+                puts "invalid command"
+                return "invalid command"
+        end 
     end
 
 end
 
-# t = Table.new
 
-def get_command(command)
-    case command
-        when /^PLACE *([0-4]), *([0-4]), *(NORTH|SOUTH|EAST|WEST)$/i 
-            match = command.match(/^PLACE *([0-4]), *([0-4]), *(NORTH|SOUTH|EAST|WEST)$/i)
-            col, row, face = match.captures
-            t = Table.new
-            t.place_robot(col.to_i, row.to_i, face)
-            puts "ok"
-            return t
-        else
-            puts "invalid command"
-            return "invalid command"
-    end 
-end
+# Main function that runs the game and gets user input
+# Valid inputs will be
+# PLACE x,y,direction
+# MOVE
+# LEFT
+# RIGHT
+# REPORT
 
 def play_game
+    t = Table.new
     print "> "
     input = gets.chomp
     while (input != "report")
-        get_command(input)
+        t.get_command(input)
         print "> "
         input = gets.chomp
     end
+    t.printTable
+    
 end
 
-# play_game
+play_game
