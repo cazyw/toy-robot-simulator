fc148c7b3775bd90a88be40d2355f9acc2885e1d
cazyw <kazeisc@gmail.com>
2017-09-10 12:43:15 +1000

    Add function to place robot on the table

--- tests/test.rb
+++ tests/test.rb
@@ -16,18 +16,39 @@ class TestRobot < Test::Unit::TestCase
         # t = Table.new
         input = "PLACE 0,0,NORTH"
         result = get_command(input)
-        assert_equal("ok", result)       
+        assert_equal(true, result.is_a?(Table))       
     end
+
     def test_input_place_invalid_direction
         # t = Table.new
         input = "PLACE 0,0,UP"
         result = get_command(input)
         assert_equal("invalid command", result)       
     end
+
     def test_input_place_invalid_arguments
         # t = Table.new
         input = "PLACE 0,,UP"
         result = get_command(input)
         assert_equal("invalid command", result)       
+    end
+
+    def test_input_place_places_robot_0_0
+        input = "PLACE 0,0,NORTH"
+        result = get_command(input)
+        assert_equal([0,0], result.pos)       
+    end
+
+    def test_input_place_places_robot_3_2
+        input = "PLACE 3,2,NORTH"
+        result = get_command(input)
+        assert_equal([3,2], result.pos)       
+        assert_equal("NORTH", result.face)       
+    end
+
+    def test_input_place_ignores_robot_5_0
+        input = "PLACE 5,0,NORTH"
+        result = get_command(input)
+        assert_equal("invalid command", result)           
     end
 end
--- toyRobot.rb
+++ toyRobot.rb
@@ -7,6 +7,16 @@ class Table
         @table = Array.new(5) { Array.new(5, "x") }
         @pos = [-1,-1]
         @face = "directionless"
+        puts "Initialise table"
+        printTable
+    end
+
+    def place_robot(col, row, f)
+        # to account for starting in bottom left corner
+        modrow = (row-4).abs
+        @table[modrow][col] = "O"
+        @pos = [col, row]
+        @face = f.downcase
         printTable
     end
 
@@ -19,18 +29,29 @@ end
 
 # t = Table.new
 
-def get_command(input)
-    case input
+def get_command(command)
+    case command
         when /^PLACE *([0-4]), *([0-4]), *(NORTH|SOUTH|EAST|WEST)$/i 
+            match = command.match(/^PLACE *([0-4]), *([0-4]), *(NORTH|SOUTH|EAST|WEST)$/i)
+            col, row, face = match.captures
+            t = Table.new
+            t.place_robot(col.to_i, row.to_i, face)
             puts "ok"
-            return "ok"
+            return t
         else
             puts "invalid command"
             return "invalid command"
     end 
 end
 
-# t = Table.new
-print "> "
-get_command(gets.chomp)
+def play_game
+    print "> "
+    input = gets.chomp
+    while (input != "report")
+        get_command(input)
+        print "> "
+        input = gets.chomp
+    end
+end
 
+# play_game
