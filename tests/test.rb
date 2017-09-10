e8d041cd9caebbb7eef6249848459e9e5744a302
cazyw <kazeisc@gmail.com>
2017-09-10 11:47:47 +1000

    Check input for PLACE command

--- tests/test.rb
+++ tests/test.rb
@@ -4,8 +4,28 @@ require "test/unit"
 class TestRobot < Test::Unit::TestCase
 
     # test the table is set up 5 x 5
-    def test_table_setup
+    def test_table_grid
         t = Table.new
         t == [["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"], ["x", "x", "x", "x", "x"]]
+        assert_equal(true, t.is_a?(Table))
+    end
+
+    def test_input_place_valid
+        # t = Table.new
+        input = "PLACE 0,0,NORTH"
+        result = get_command(input)
+        assert_equal("ok", result)       
+    end
+    def test_input_place_invalid_direction
+        # t = Table.new
+        input = "PLACE 0,0,UP"
+        result = get_command(input)
+        assert_equal("invalid command", result)       
+    end
+    def test_input_place_invalid_arguments
+        # t = Table.new
+        input = "PLACE 0,,UP"
+        result = get_command(input)
+        assert_equal("invalid command", result)       
     end
 end
--- toyRobot.rb
+++ toyRobot.rb
@@ -1,12 +1,31 @@
 # Toy Robot Simulator
 
 class Table
+    attr_reader :table
     # Setup the table
     def initialize
         @table = Array.new(5) { Array.new(5, "x") }
         puts @table.map { |x| x.join(" ") }
     end
 
+    
+
 end
 
-t = Table.new
+# t = Table.new
+
+def get_command(input)
+    case input
+        when /^PLACE *([0-4]), *([0-4]), *(NORTH|SOUTH|EAST|WEST)$/i 
+            puts "ok"
+            return "ok"
+        else
+            puts "invalid command"
+            return "invalid command"
+    end 
+end
+
+# t = Table.new
+print "> "
+get_command(gets.chomp)
+
