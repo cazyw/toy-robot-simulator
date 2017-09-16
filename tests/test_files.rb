d003723d3b8c3627d28275e4d9787e5dd01dc8d6
cazyw <kazeisc@gmail.com>
2017-09-16 19:24:11 +1000

    Add test file that tests the test files provided and begin README update

---
+++ tests/test_files.rb
@@ -0,0 +1,56 @@
+$: << "../lib"
+require "game"
+require "test/unit"
+
+class TestFiles < Test::Unit::TestCase
+
+    def setup
+        @game = Game.new
+    end
+
+
+    def test_file_input_results
+            File.open("./tests/robot_commands_1.txt") do |f|
+                commands = f.readlines
+                commands.each do |x|
+                    next if x.chomp == ""
+                    @game = @game.commands(x.chomp)
+                end
+            end
+            assert_equal([0, 1], @game.robot.robot[:pos])
+            assert_equal("NORTH", @game.robot.robot[:dir])
+
+            @game = Game.new
+            File.open("./tests/robot_commands_2.txt") do |f|
+                commands = f.readlines
+                commands.each do |x|
+                    next if x.chomp == ""
+                    @game = @game.commands(x.chomp)
+                end
+            end
+            assert_equal([0, 0], @game.robot.robot[:pos])
+            assert_equal("WEST", @game.robot.robot[:dir])
+
+            @game = Game.new
+            File.open("./tests/robot_commands_3.txt") do |f|
+                commands = f.readlines
+                commands.each do |x|
+                    next if x.chomp == ""
+                    @game = @game.commands(x.chomp)
+                end
+            end
+            assert_equal([3, 3], @game.robot.robot[:pos])
+            assert_equal("NORTH", @game.robot.robot[:dir])
+
+            @game = Game.new
+            File.open("./tests/robot_commands_4.txt") do |f|
+                commands = f.readlines
+                commands.each do |x|
+                    next if x.chomp == ""
+                    @game = @game.commands(x.chomp)
+                end
+            end
+            assert_equal([3, 3], @game.robot.robot[:pos])
+            assert_equal("WEST", @game.robot.robot[:dir])
+    end
+end
