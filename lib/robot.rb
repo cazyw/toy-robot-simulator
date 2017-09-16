eac191e84fa69d5de549e3ed4ae226fe9df026d9
cazyw <kazeisc@gmail.com>
2017-09-16 12:06:59 +1000

    Add method to change robot position and direction

--- lib/robot.rb
+++ lib/robot.rb
@@ -12,20 +12,21 @@ class Robot
     end
 
     def robot_active?
-        !!@robot[:pos][0] === -1
+        @robot[:pos][0] != -1
     end
 
-    def move_robot
-
+    def set_robot_position(row, col)
+        @robot[:pos] = [row, col]
+        return self
     end
 
-    def turn_robot
+    def set_robot_direction(direction)
+        @robot[:dir] = direction
+        return self
     end
 
     def print_robot
         self.robot_active? ? "Output: #{@robot[:pos]} facing #{@robot[:dir]}" : "robot is inactive"
-        # puts "Output: #{@robot[:pos]} facing #{@robot[:dir]}"
-        # @robot_placed ? "Output: (#{@pos[0]},#{@pos[1]}) facing #{@face}" : "Robot has not been placed yet"
     end
 
 end
--- tests/test_robot.rb
+++ tests/test_robot.rb
@@ -14,10 +14,19 @@ class TestRobot < Test::Unit::TestCase
         assert_equal("", @r.robot[:dir])
     end
 
-    def test_robot_active
+    def test_robot_inactive_on_setup
         assert_equal(false, @r.robot_active?)
         assert_equal("robot is inactive", @r.print_robot)
     end
 
-
+    def test_robot_active_on_placed
+        @r.set_robot_position(0, 0)
+        @r.set_robot_direction("NORTH")
+        assert_equal(true, @r.robot_active?)
+        assert_equal("Output: [0, 0] facing NORTH", @r.print_robot)
+        @r.set_robot_position(3, 2)
+        @r.set_robot_direction("WEST")
+        assert_equal(true, @r.robot_active?)
+        assert_equal("Output: [3, 2] facing WEST", @r.print_robot)
+    end
 end
